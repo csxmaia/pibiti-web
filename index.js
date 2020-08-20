67b0f572858a48d281df5fc5efad04a616d16358
Cristhian <cristhian.smaia@gmail.com>
2020-08-19 21:23:49 -0300

    initial configs

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
-const express = require('express'); 
-const {PythonShell} = require('python-shell');
-
-const multer = require('multer');
 const path = require('path');
+const {PythonShell} = require('python-shell');
+require('dotenv').config();
 
+const express = require('express'); 
 const app = express()
 http = require('http').createServer(app)
 io = require('socket.io')(http);
+
+//static files in public folder
 app.use(express.static(__dirname + '/public'));
 
+//index; select a file to upload
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+//File upload
 app.post('/run', function(req, res, next){
   var formidable = require('formidable');
   var fs = require('fs');
   var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
       // console.log(files.feature.path)
+      //change file name and path
       var oldpath = files.feature.path;
-      var newpath = '../PIBITI/features/' + 'data.txt';
+      var newpath = `${process.env.MAIN_SCRIPT_PATH}/features/` + 'data.txt';
+      console.log(newpath)
       fs.rename(oldpath, newpath, function (err) {
         if (err) throw err;
-        // res.write('File uploaded and moved!');
+        // so throw feature script file
         res.sendFile(__dirname + '/feature.html');
       });
     });
@@ -32,24 +37,31 @@ app.post('/run', function(req, res, next){
 //   console.log("testeeeeeeeeeeeeeeeeeeeeeeee")
 // });
 
+// let shell = new PythonShell('../PIBITI/libsvm-3.24/tools/easy.py', { mode: 'text', args: ['vetTreino.txt', 'vetor_diss_teste.txt']});
 
 io.of("/run").on('connection', (socket) => {
   
   console.log('a user connected');
   
-  let shell = new PythonShell('../PIBITI/importar_arquivo.py', { mode: 'text'});
-  // let shell = new PythonShell('../PIBITI/libsvm-3.24/tools/easy.py', { mode: 'text', args: ['vetTreino.txt', 'vetor_diss_teste.txt']});
+  let shell = new PythonShell(`${process.env.MAIN_SCRIPT_PATH}/importar_arquivo.py`, { mode: 'text'});
   shell.on('message', function (message) {
+    //get print ad turn into String
     var output = String(message);
-    var isget = output.split(" ")
-    console.log(isget)
-    if(isget[0] === "get") {
-      io.sockets.emit('get', { data: isget[1]})
-      if(isget[1] === "class"){
+    //Split print; to search Actions
+    var isAction = output.split(" ")
+    console.log(isAction)
+    //if action emit action to action perform with the data (the real action)
+
+    if(isAction[0] === "action") {
+      console.log("emitindo "+isAction[1])
+      io.sockets.emit('action', { data: isAction[1]})
+      //class is the one of all who need input in script by web, so this special if
+      if(isAction[1] === "class"){
         socket.on('selector', function(id) {
           shell.send(id)
         })
       }
+    //if false; return the print as output text
     }else{
       io.sockets.emit('output-update', { data: output });
     }
