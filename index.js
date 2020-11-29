690d693d6c0c9a00ca2c8e267d96d32c8a9988e9
Cristhian <cristhian.smaia@gmail.com>
2020-11-29 15:15:51 -0300

    implementacao de busca e pessoas

--- index.js
+++ index.js
@@ -26,7 +26,88 @@ app.use(express.static(__dirname + "/public"));
 
 //index; select a file to upload
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/pages/index.html");
+});
+
+app.get("/classificador-base", function (req, res) {
+  res.sendFile(__dirname + "/pages/base-intro.html");
+});
+
+app.get("/classificador-pessoa", function (req, res) {
+  res.sendFile(__dirname + "/pages/pessoa-intro.html");
+});
+
+app.get("/classificador-pessoa/upload/:id", function (req, res) {
+  
+  res.sendFile(__dirname + "/pages/pessoa_upload.html");
+});
+
+app.get("/classificador-pessoa/:id", function (req, res) {
+  //2 - incluir pessoa por gravação 3 - gravar uma amostra para teste
+  res.sendFile(__dirname + "/pages/pessoa_gravacao.html");
+});
+
+app.post("/classificador-pessoa/:id", function (req, res) {
+  const {id} = req.params;
+  //1 - incluir pessoa por upload 4 - upar uma amostra já gravada para teste
+  if(id==1 || id==4){
+    var formidable = require("formidable");
+    var fs = require("fs");
+    var form = new formidable({multiples: true})
+    const arquivos = fs.readdirSync(`${process.env.MAIN_SCRIPT_PATH}/PIBITI/brSD_audiofeat_audios`);
+    const total_pessoas = arquivos.length;
+    const ultimo_audio = total_pessoas*5;
+
+    try {
+      fs.rmdir(`${process.env.MAIN_SCRIPT_PATH}/PIBITI/audios_gravados`, { recursive: true }, (err) => {
+        if (err) {
+          throw err;
+        }
+      })
+      console.log("dir audios_gravados excluido")
+    } catch (error) {
+      console.log("Error deletar dir audios_gravados " + error)      
+    }
+    console.log(ultimo_audio)
+
+    try {
+      fs.unlinkSync(`${process.env.MAIN_SCRIPT_PATH}/audios_upados_teste/entrada_upada_teste.wav`)
+    } catch (error) {
+      console.log("entrada_upada_teste nao existe para exclusao")
+    }
+
+    form.parse(req, function (err, fields, files) {
+      if(id==1){
+        var oldPath = {
+          audio1: files.audio1.path,
+          audio2: files.audio2.path,
+          audio3: files.audio3.path,
+          audio4: files.audio4.path,
+          audio5: files.audio5.path,
+        }
+        var newPath = {}
+        Object.keys(oldPath).map(function(name, index) {
+          newPath = {...newPath, [name]:`${process.env.MAIN_SCRIPT_PATH}/PIBITI/audios_gravados/${(ultimo_audio+1)+index}.wav`}
+        });
+        
+        // Object.keys(newPath).map(function(name, index) {
+        //   fs.rename(oldPath[name], newPath[name], function (err) {
+        //     if (err) throw err;
+        //   });
+        // })
+        res.sendFile(__dirname + "/pages/pessoa.html");
+      }
+      if(id==4){
+        var oldPath = files.audio1.path
+        var newPath = `${process.env.MAIN_SCRIPT_PATH}/audios_upados_teste/entrada_upada_teste.wav`
+        fs.rename(oldPath, newPath, function (err) {
+          if (err) throw err;
+        });
+        res.sendFile(__dirname + "/pages/pessoa.html");
+      }
+    })
+
+  }
 });
 
 //File upload
@@ -80,7 +161,85 @@ app.post("/run", function (req, res, next) {
     //   }
     //   console.log("existe")
     // })
-    res.sendFile(__dirname + "/feature.html");
+    res.sendFile(__dirname + "/pages/base.html");
+  });
+});
+
+io.of("/pessoa-gravacao").on("connection", function (socket) {
+  console.log("a user connected");
+
+  var options = {
+    mode: 'text',
+    // pythonPath: 'C:/Python38/python',
+  }
+
+  var shell = new PythonShell(
+    `${process.env.MAIN_SCRIPT_PATH}/rec_voz.py`,
+    options
+  );
+
+  shell.on("message", function (message) {
+    output = String(message);
+    isAction = output.split(" && ");
+    console.log(output)
+    // console.log(isAction);
+    //if action emit action to action, perform with the data (the real action)
+
+    if (isAction[0] === "action") {
+      if (isAction[1] === "text") {
+        socket.emit("etapa-text", {
+          data: isAction[2],
+        });
+      }
+    }
+  })
+
+  socket.once("envia-comando", function (id) {
+    shell.send(parseInt(id));
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+    shell.childProcess.kill("SIGINT");
+  });
+});
+
+io.of("/pessoa-upload").on("connection", function (socket) {
+  console.log("a user connected");
+
+  var options = {
+    mode: 'text',
+    // pythonPath: 'C:/Python38/python',
+  }
+
+  var shell = new PythonShell(
+    `${process.env.MAIN_SCRIPT_PATH}/rec_voz.py`,
+    options
+  );
+
+  shell.on("message", function (message) {
+    output = String(message);
+    isAction = output.split(" && ");
+    console.log(output)
+    // console.log(isAction);
+    //if action emit action to action, perform with the data (the real action)
+
+    if (isAction[0] === "action") {
+      if (isAction[1] === "result") {
+        socket.emit("output", {
+          data: isAction[2],
+        });
+      }
+    }
+  })
+
+  socket.once("envia-comando", function (id) {
+    shell.send(parseInt(id));
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+    shell.childProcess.kill("SIGINT");
   });
 });
 
